999e8e9b58920e3ba814b03fa09fb92e132e098c
Patrick White <patrick.white@youi.tv>
2019-05-28 17:15:03 -0400

    PS4 fixes for cmake run compile checks, various missing headers and functionality in folly

--- folly/Traits.h
+++ folly/Traits.h
@@ -476,10 +476,10 @@ namespace traits_detail_IsNothrowSwappable {
 // enabled.
 template <typename T>
 using IsNothrowSwappable = std::is_nothrow_swappable<T>;
-#elif _CPPLIB_VER
-// MSVC defines the base even if C++17 is disabled, and MSVC has
-// issues with our fallback implementation due to over-eager
-// evaluation of noexcept.
+#elif _CPPLIB_VER  && !defined(__ORBIS__)
+// MSVC 2015+ defines the base even if C++17 is disabled, and
+// MSVC 2015 has issues with our fallback implementation due to
+// over-eager evaluation of noexcept.
 template <typename T>
 using IsNothrowSwappable = std::_Is_nothrow_swappable<T>;
 #else
